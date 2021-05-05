3fb5525fff910bbb9febf138c4c075e99739201a
JorgeRojas827 <jorgerojaspoma09@gmail.com>
2021-05-04 21:38:04 -0500

    Template Done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import './app.scss'
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container p-5">
+        <h3 className = "text-red-600">Hello world!</h3>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
